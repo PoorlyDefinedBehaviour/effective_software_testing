9c4ad53e8d0c5cb612f4e197c6d53dbeb31777ab
PoorlyDefinedBehaviour <brunotj2015@hotmail.com>
2022-02-25 00:16:05 -0300

    feat(planning_poker): use proptest

--- planning_poker/src/main.rs
+++ planning_poker/src/main.rs
@@ -1,12 +1,12 @@
 use thiserror::Error;
 
-#[derive(Debug, Error, PartialEq)]
+#[derive(Debug, Error, PartialEq, Clone)]
 enum PlanningPokerError {
   #[error("planning poker requires at least two players. got {got:?}")]
   NotEnoughPlayers { got: usize },
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 struct Estimate {
   player: Player,
   estimate: usize,
@@ -14,7 +14,9 @@ struct Estimate {
 
 type Player = String;
 
-fn find_extremes(estimates: &[Estimate]) -> Result<(&Estimate, &Estimate), PlanningPokerError> {
+fn find_extremes(
+  estimates: &[Estimate],
+) -> Result<Option<(&Estimate, &Estimate)>, PlanningPokerError> {
   if estimates.len() < 2 {
     return Err(PlanningPokerError::NotEnoughPlayers {
       got: estimates.len(),
@@ -33,7 +35,12 @@ fn find_extremes(estimates: &[Estimate]) -> Result<(&Estimate, &Estimate), Plann
     }
   }
 
-  Ok((lowest, highest))
+  // Everyone had the same estimates.
+  if lowest.estimate == highest.estimate {
+    return Ok(None);
+  }
+
+  Ok(Some((lowest, highest)))
 }
 
 fn main() {
@@ -43,6 +50,38 @@ fn main() {
 #[cfg(test)]
 mod tests {
   use super::*;
+
+  use proptest::prelude::*;
+
+  proptest! {
+    #[test]
+   fn returns_players_with_lowest_and_highest_estimates(mut xs: Vec<usize>) {
+      xs.push(1);
+      xs.push(usize::MAX);
+
+      let xs: Vec<Estimate> = xs.into_iter().filter(|x|*x > 0).map(|i| Estimate {
+        player: i.to_string(),
+        estimate:i,
+      }).collect();
+
+      let actual = find_extremes(&xs).unwrap().unwrap().clone();
+
+      assert_eq!(
+        Estimate{
+          player: Player::from("1"),
+          estimate: 1,
+        },
+        actual.0.clone()
+      );
+      assert_eq!(
+        Estimate{
+          player: usize::MAX.to_string(),
+          estimate: usize::MAX,
+        },
+        actual.1.clone()
+      );
+    }
+  }
 
   #[test]
   fn at_least_two_estimates_are_required() {
@@ -63,7 +102,7 @@ mod tests {
   }
 
   #[test]
-  fn returns_players_with_lowest_and_highest_estimates_when_estimates_are_the_same() {
+  fn returns_no_playes_when_estimates_are_equal() {
     let estimates = vec![
       Estimate {
         player: Player::from("player_1"),
@@ -79,38 +118,6 @@ mod tests {
       },
     ];
 
-    assert_eq!(
-      Ok((&estimates[0], &estimates[1])),
-      find_extremes(&estimates)
-    );
-  }
-
-  #[test]
-  fn returns_players_with_lowest_and_highest_estimates_are_not_the_same() {
-    let mut estimates = vec![
-      Estimate {
-        player: Player::from("player_1"),
-        estimate: 4,
-      },
-      Estimate {
-        player: Player::from("player_2"),
-        estimate: 2,
-      },
-    ];
-
-    assert_eq!(
-      Ok((&estimates[1], &estimates[0])),
-      find_extremes(&estimates)
-    );
-
-    estimates.push(Estimate {
-      player: Player::from("player_3"),
-      estimate: 1,
-    });
-
-    assert_eq!(
-      Ok((&estimates[2], &estimates[0])),
-      find_extremes(&estimates)
-    );
+    assert_eq!(Ok(None), find_extremes(&estimates));
   }
 }
